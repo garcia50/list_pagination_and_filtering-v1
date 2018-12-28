c6e35d8b50c7e5c6da447be10fdd8b031e0b3146
Luis Garcia <garcia50@yahoo.com>
2018-12-28 17:16:27 -0500

    Modify comment

--- js/script.js
+++ js/script.js
@@ -7,14 +7,7 @@ FSJS project 2 - List Filter and Pagination
 
 
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
+Create constants and variables that will later be used and manipulated.
 ***/
 const pageParentDiv = document.getElementsByClassName("page") 
 const pageHeader = document.getElementsByClassName("page-header cf");
